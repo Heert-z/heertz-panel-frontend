2c593314265312ed8b72a1b9d5b9070850a4f149
Tom <production64times@gmail.com>
2021-05-02 19:35:09 +0200

    first front commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,17 @@
 <template>
   <v-main>
+    <v-container fill-height>
+      <v-row justify="center" align="center">
+        <div id="landing">
+          <v-img width="300px" src="../assets/logo.png" alt="heertz-logo"/>
+            <v-container class="text-h2 text-uppercase">
+              <span class="text--secondary font-weight-bold">Heert'z</span>
+              <span class="text--secondary">bot</span>
+            </v-container>
+          <p>The bot you need !</p>
+        </div>
+      </v-row>
+    </v-container>
   </v-main>
 </template>
 
@@ -9,3 +21,14 @@ export default {
   components: {}
 }
 </script>
+<style scoped lang="scss">
+
+#landing {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+}
+
+</style>
